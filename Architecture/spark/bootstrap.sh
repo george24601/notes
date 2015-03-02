ab6f0f03f744bd57673d4c4e3976209caf7359f8
George <movingapple@gmail.com>
2015-03-02 17:38:41 -0500

    HDFS setup

--- Architecture/spark/bootstrap.sh
+++ Architecture/spark/bootstrap.sh
@@ -1,21 +1,21 @@
-#install java
-JDK_NAME=jdk1.8.0_31
-
-tar -xvf jdk-8u31-linux-x64.gz
-sudo mkdir -p /usr/lib/jvm
-sudo mv ./$JDK_NAME/usr/lib/jvm/
-
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_31/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_31/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_31/bin/javaws" 1
-
-sudo chmod a+x /usr/bin/java
-sudo chmod a+x /usr/bin/javac
-sudo chmod a+x /usr/bin/javaws
-sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_31
+sudo apt-get update
+#or use openjdk
+sudo apt-get install openjdk-7-jdk
 
 #install spark
 tar -xvf spark-1.2.1-bin-hadoop2.4.tgz
 
+#spark master listening
+sudo ufw allow 7077
+
 #install hadoop
+sudo apt-get install ssh
 tar -xvf hadoop-2.6.0.tar.gz
+
+#hadoop name node listening
+sudo ufw allow 8020
+
+#hadoop resource node listening
+sudo ufw allow 8031
+
+#install hadoop-env.sh
--- Architecture/spark/start.sh
+++
@@ -1,4 +0,0 @@
-SPARK_MASTER=
-
-#start in worker node
-./spark-1.2.1-bin-hadoop2.4/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER:7077
