82050cffeb1dc71bccf2503f00a9b3cccee46ec6
George <movingapple@gmail.com>
2015-03-12 14:09:10 -0400

    git notes

--- Architecture/spark/bootstrap.sh
+++ Architecture/spark/bootstrap.sh
@@ -1,12 +1,7 @@
-sudo apt-get update
 #or use openjdk
 sudo apt-get install openjdk-7-jdk
 
-#install spark
-tar -xvf spark-1.2.1-bin-hadoop2.4.tgz
-tar -xvf hadoop-2.6.0.tar.gz
-
-#open all ports
+#open all tcp/udp ports
 sudo apt-get install ufw
 sudo ufw enabled
 sudo ufw allow 1:65535/tcp
@@ -27,19 +22,19 @@ sudo ufw allow 8031
 #add mounting points to data nodes, spark recommends 4-8 mounting points, i.e,
 
 sudo chmod 777 -R /mnt/disk1
-mkdir /mnt/disk1/dn
 
 sudo chmod 777 -R /mnt/disk2
-mkdir /mnt/disk2/dn
 
 sudo chmod 777 -R /mnt/disk3
-mkdir /mnt/disk3/dn
 
 sudo chmod 777 -R /mnt/disk4
-mkdir /mnt/disk4/dn
 
 mkdir /mnt/disk1/sparkTmp
 mkdir /mnt/disk2/sparkTmp
 mkdir /mnt/disk3/sparkTmp
 mkdir /mnt/disk4/sparkTmp
-vim spark-1.2.1-bin-hadoop2.4/conf/spark-env.sh
+
+mkdir /mnt/disk1/dn
+mkdir /mnt/disk2/dn
+mkdir /mnt/disk3/dn
+mkdir /mnt/disk4/dn
--- Architecture/spark/upload.sh
+++ Architecture/spark/upload.sh
@@ -2,12 +2,13 @@ SSH_USER=
 DEST_HOST=
 
 #setup SSH
-ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
-cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
-ssh-copy-id {user}@{data node hostname}
+#ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
+#cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+ssh-copy-id $SSH_USER@$DEST_HOST
 
 #transfer files
 scp  -r hadoop-2.6.0/ $SSH_USER@$DEST_HOST:hadoop-2.6.0/
 scp -r spark-1.2.1-bin-hadoop2.4/ $SSH_USER@$DEST_HOST:spark-1.2.1-bin-hadoop2.4/
+scp -r bootstrap.sh $SSH_USER@$DEST_HOST:bootstrap.sh
 
 ssh  $SSH_USER@$DEST_HOST
