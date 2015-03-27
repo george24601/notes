9abd3998e3ebe1d3c04c6ebb1ccfc5f6a9862049
unknown <movingapple@gmail.com>
2015-03-27 11:08:09 -0400

    updated kafka producer/consumer with node client

--- Specific/Kafka/c.js
+++ Specific/Kafka/c.js
@@ -1,33 +1,40 @@
 var subTopic = 'test'; //TODO:which topic are you subscribing?
 var topicPartition = 0; //TODO: keep in mind each partition can be consumed by only one instance inside each consumer group 
+var useOffset = false;
 var logOffset = 6000; //TODO: auto commit is off for now
 var zookeeperHandle = 'localhost:2181/';
 
-function messageAction(message){
-    console.log(message); //TODO: your concrete function goes here
-};
-
-
-
 var kafka = require('kafka-node');
 var consumer = new kafka.Consumer(
-	new kafka.Client(zookeeperHandle),
-        [
-            { topic: subTopic, partition: topicPartition, offset: logOffset }
-        ],
-        {
-            fromOffset: true,
-            autoCommit: false 
-        }
-    );
+		new kafka.Client(zookeeperHandle),
+		[
+		{ topic: subTopic, partition: topicPartition, offset: logOffset }
+		],
+		{autoCommit: false, fromOffset: useOffset}
+		);
 
-consumer.on('message', messageAction);
 
 consumer.on('offsetOutOfRange', function (err) {
-	console.log('OFFSET OUT OF RANGE');
-});
+		console.log('OFFSET OUT OF RANGE');
+		});
 
 consumer.on('error', function (err) {
-	console.log('CONSUMER ERROR');
-});
+		console.log('CONSUMER ERROR');
+		});
 
+///TODO: replace this with your logic goes here,  
+function messageAction(message){
+	var offset = message['offset'];
+	console.log(message['offset']); 
+
+	if (false){
+
+		consumer.commit(function (err, data) { //TODO: you need to explicitly commit your current consumer's offset, so that next time it picks up from where it is left off
+				if (err) throw err;
+
+				console.log(data);
+				});
+	}
+};
+
+consumer.on('message', messageAction);
--- Specific/Kafka/p.js
+++ Specific/Kafka/p.js
@@ -1,28 +1,37 @@
 var subTopic = 'test'; //TODO:which topic are you publishing to?
 var topicPartition = 0;//keep in mind each partition is consumed by one instance in each consumer group
-var messagesToSend = ['from node']; //TODO: your JSON goes here
+var filePath = process.argv[2];
+var fs = require('fs');
 
 function sent (err, data) {
-        console.log('Offset at ' + data[subTopic][topicPartition]); //TODO: your concrete logic goes here
+	console.log('Offset at ' + data[subTopic][topicPartition]); //TODO: your concrete logic goes here
 };
-
 
 var zookeeperHandle = 'localhost:2181/'
 
-
 var kafka = require('kafka-node');
 var producer = new kafka.Producer(new kafka.Client(zookeeperHandle)); //do we need new?
-
-var payloads = [{
-   topic: subTopic, 
-   messages: messagesToSend, 
-   partition: topicPartition
-}];//just one request for now, probably needs more
-
 producer.on('ready', function () {
-    producer.send(payloads, sent);
-});
+		fs.readFile(filePath, function (err, data) {
+			var messagesToSend;
+			if (err) throw err;
+
+			messagesToSend = data.toString().split('\n');
+			
+			//console.log (messagesToSend);
+
+			var payloads = [{
+				topic: subTopic,
+				messages: messagesToSend, 
+				partition: topicPartition
+				}];//just one request for now, probably needs more
+
+			producer.send(payloads, sent);
+}
+);
+
+		});
 
 producer.on('error', function (err) {
-	console.log ('PRODUCER ERROR');
-});
+		console.log ('PRODUCER ERROR');
+		});
