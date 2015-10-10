1cbf9ef4c2129286e7582286f22a930269147015
George <movingapple@gmail.com>
2015-10-10 12:12:44 -0400

    confluent

--- Specific/confluent/script.sh
+++ Specific/confluent/script.sh
@@ -3,10 +3,17 @@ wget http://packages.confluent.io/archive/1.0/confluent-1.0.1-2.10.4.zip
 unzip confluent-1.0.1-2.10.4.zip
 cd confluent-1.0.1
 
-#start
+#start ZK in 1st session 
 ./bin/zookeeper-server-start ./etc/kafka/zookeeper.properties
+
+#start Kafka in 2nd session, requires running ZK
 ./bin/kafka-server-start ./etc/kafka/server.properties
+
+#start SR in 3rd session, requires running ZK and Kafka 
 ./bin/schema-registry-start ./etc/schema-registry/schema-registry.properties
+
+#start REST proxy in 4th session, requires running SR
+./bin/kafka-rest-start ./etc/kafka-rest/kafka-rest.properties
 
 #send to topic from console, if the schema is incompatible, it will error out
 ./bin/kafka-avro-console-producer \
