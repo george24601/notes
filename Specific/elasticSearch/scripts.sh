d0627e62faa839dcf6452eceaadbfb4dc15ee791
George Li <movingapple@gmail.com>
2015-12-07 10:14:37 -0500

    simple date range query works

--- Specific/elasticSearch/scripts.sh
+++ Specific/elasticSearch/scripts.sh
@@ -1,1 +1,10 @@
 curl -d @qPL.json http://localhost:9200/spark/_search?pretty 
+
+curl -d @filter.json http://localhost:9200/spark/confluent/_search?pretty 
+
+curl -d @filterAgg.json http://localhost:9200/spark/confluent/_search?pretty 
+
+curl -d @dateFilterAgg.json http://localhost:9200/spark/confluent/_search?pretty 
+
+#check field type
+curl -XGET 'http://localhost:9200/spark/_mapping/confluent/field/dateInserted'
