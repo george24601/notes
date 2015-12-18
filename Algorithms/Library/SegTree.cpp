5a345c8b7a4dfa17117e79a526340023932d3ef6
George Li <movingapple@gmail.com>
2015-12-18 16:22:04 -0500

    cleaned up tree-based seg tree implementation

--- Algorithms/Library/SegTree.cpp
+++ Algorithms/Library/SegTree.cpp
@@ -31,128 +31,54 @@ int nums[MaxN];
 
 
 class SegTree {
-private:
-	int start;
-	int end;
+  private:
+    int start;
+    int end;
 
-	SegTree() {
-		start = -1;
-		end = -1;
-		left = NULL;
-		right = NULL;
-	}
+    SegTree() {
+      start = -1;
+      end = -1;
+      left = NULL;
+      right = NULL;
+    }
 
-public:
-	SegTree* left;
-	SegTree* right;
+  public:
+    SegTree* left;
+    SegTree* right;
 
-	PII le;
-	PII re;
-	PII me;
+    //Node payload goes here
 
-	SegTree(int s, int e, SegTree* l, SegTree* r, PII lec, PII rec, PII mec) {
-		start = s;
-		end = e;
-		left = l;
-		right = r;
+    SegTree(int s, int e, SegTree* l, SegTree* r) {
+      start = s;
+      end = e;
+      left = l;
+      right = r;
 
-		le = lec;
-		re = rec;
-		me = mec;
-	}
+      //payload assignment here
+    }
 
-	~SegTree() {
-		delete left;
-		delete right;
-	}
+    ~SegTree() {
+      delete left;
+      delete right;
+    }
 
-	PII Q(int s, int e) {
-
-		//Assumption s >= start, e <= end;
-
-		if(s == start && e == end)
-			return me;
-
-		if(left != NULL && right != NULL)
-		{
-			if(s >= right->start)
-				return right->Q(s, e);
-
-			if(e <= left-> end)
-				return left->Q(s, e);
-
-			int mid = (start + end)/2;
-			PII leftQ = left -> Q(s, mid);
-			PII rightQ = right ->Q(mid + 1, e);
-
-			PII toReturn = leftQ.second > rightQ.second? leftQ: rightQ;
-
-			if(nums[mid] == nums[mid + 1])
-			{
-				int mlc = min(mid - s + 1, left->re.second); //need to process counts
-				int mrc = min(e - mid, right->le.second);//need to process counts;
-
-				if(toReturn.second < mlc + mrc)
-					toReturn = PII (nums[mid], mlc + mrc);
-			}
-
-			return toReturn;
-
-		}else if(left != NULL)
-		{
-			return left->Q(s, e);
-		}
-		else
-		{
-			return right->Q(s, e);
-		}
-
-		//if s == start && e == end => directly return this seg's conclusion
-		//if s < right->start, get left q //NULL case
-		//left candidate: leftQ, left's RC
-		//similarly if e > left -> end, get right Q
-		//right candidate: rightQ, right's LC
-		//merge: leftQ, right Q, left's RC + right's LC if possible
-
-		//return of winner and frequence
-
-	}
+    //Query here, note that the tree is binary,i.e., either two chilren or here
 
 };
 
 SegTree* BuildTree(int start, int end) {
-	if (start == end) {
-		PII single = PII (nums[start], 1);
-		return new SegTree(start, end, NULL, NULL, single, single, single);
-	}
+  if (start == end) {
+    return new SegTree(start, end, NULL, NULL);
+  }
 
-	int mid = (start + end) / 2;
+  int mid = (start + end) / 2;
 
-	SegTree* l = BuildTree(start, mid);
+  SegTree* l = BuildTree(start, mid);
 
-	SegTree* r = BuildTree(mid + 1, end);
+  SegTree* r = BuildTree(mid + 1, end);
 
-	int	 newLc = l->le.second;
-	int	 newRc = r->re.second;
+  //calculate combined payload here
 
-	PII newM = l->me.second > r->me.second? l->me : r-> me;
-
-	//calc new left
-	if(nums[mid] == nums[mid + 1])
-	{
-		int newMidc = l->re.second + r-> le.second;
-
-		if(nums[start] == nums[mid + 1])
-			newLc += r->le.second;
-
-		if(nums[mid] == nums[end])
-			newRc += l->re.second;
-
-		if(newMidc > newM.second)
-			newM = PII (nums[mid], newMidc);
-	}
-
-	//merge val here
-	return new SegTree(start, end, l, r, PII(nums[start], newLc),PII(nums[end], newRc), newM);
+  return new SegTree(start, end, l, r);
 
 }
