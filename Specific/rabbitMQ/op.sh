adb595f161e64be065f45c8cf3098ca4bc4485a0
George Li <george.li.24601@gmail.com>
2016-04-23 23:17:57 -0400

    paxos notes

---
+++ Specific/rabbitMQ/op.sh
@@ -0,0 +1,13 @@
+#standard port 5672
+rabbitmq-server
+
+#create guest with password guest
+
+rabbitmqctl status
+
+launchctl limit
+
+#the relationship between exchange and a queue
+rabbitmqctl list_bindings
+
+rabbitmqctl list_exchanges
