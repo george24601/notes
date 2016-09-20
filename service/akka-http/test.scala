65a938f5aa5e96cb15b56581aecfbad408d491dd
George Li <george.li.24601@gmail.com>
2016-09-20 19:45:03 -0400

    akka notes

--- service/akka-http/test.scala
+++ service/akka-http/test.scala
@@ -11,3 +11,38 @@ Put() ~> Route.seal(smallRoute) ~> check {
   status === StatusCodes.MethodNotAllowed
   responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
 }
+
+//Sync testing for atomic works
+val actorRef = TestActorRef[MyActor]
+val actor = actorRef.underlyingActor
+
+val future = actorRef ? Say42
+val Success(result: Int) = future.value.get
+result should be(42)
+
+//we expect this actorRef to throw exception anyway
+intercept[IllegalArgumentException] { actorRef.receive("hello") }
+
+//async testing for sequence of incoming events
+//
+
+//implict sender means testActor will be the sender reference
+class MySpec() extends TestKit(ActorSystem("MySpec")) with ImplicitSender
+with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "An Echo actor" must {
+
+    "send back messages unchanged" in {
+      val echo = system.actorOf(TestActors.echoActorProps)
+      echo ! "hello world"
+      expectMsg("hello world")
+    }
+
+  }
+}
+
+//Expecting Log Messages to handle exception
