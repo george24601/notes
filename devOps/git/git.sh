63381114a0cd6547e60bd9c16e7e090c1db4bb6c
George Li <george.li.24601@gmail.com>
2017-01-12 16:19:49 -0500

    pas notes

--- devOps/git/git.sh
+++ devOps/git/git.sh
@@ -38,3 +38,11 @@ git push --tags
 
 #compare commit f7b5's parent with f7b5
 git diff f7b5^ f7b5
+
+#From the feature branch, push a single commit on top of the master with no merge commit
+#on master
+git merge --squash $FEATURE_BRANCH
+git commit -m "$COMMIT_MESSAGE"
+#pull master again to ensure there is no conflict between current master and your commit
+git pull --rebase 
+git push origin master
