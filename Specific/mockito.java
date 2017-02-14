ed6f1849016329c049c554cf0f955ad328350995
George Li <george.li.24601@gmail.com>
2017-02-14 17:50:45 -0500

    notes from the past week

---
+++ Specific/mockito.java
@@ -0,0 +1,19 @@
+
+class Sample{
+
+  public void ex() {
+
+    //verify simple invocation on mock
+    mockedList.size();
+    verify(mockedList).size();
+
+//verify an interaction has not occurred
+    List<String> mockedList = mock(MyList.class);
+    mockedList.size();
+    verify(mockedList, never()).clear();
+
+    //verify number of interactions with mock
+    mockedList.size();
+    verify(mockedList, times(1)).size();
+  }
+}
