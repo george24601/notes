62883e6919b0a0eef8e2abeb4de27f6887482769
George Li <george.li@instacart.com>
2021-08-15 19:53:04 -0400

    js notes

--- js/0.js
+++ js/0.js
@@ -1,4 +1,14 @@
-import React, { useState, useEffect } from 'react';
+//conditionally add properties
+var component = (
+  <div
+    value="foo"
+    { ...( condition && { disabled: true } ) } />
+);
+
+
+<Modal {...this.props} title='Modal heading' animation={false}>
+//is equivalent to
+<Modal a={this.props.a} b={this.props.b} title='Modal heading' animation={false}>
 
 //This is one of the traits of async functions — their return values are guaranteed to be converted to promises.
 let hello = async () => { return "Hello" };
@@ -21,22 +31,6 @@ function useFriendStatus(friendID) {
   return isOnline;
 }
 
-// The defaultValue argument is only used when a component does not have a matching Provider above it in the tree. This default value can be helpful for testing components in isolation without wrapping them. Note: passing undefined as a Provider value does not cause consuming components to use defaultValue.
-const MyContext = React.createContext(defaultValue);
-
-
-
-  // Correct
-this.setState((state, props) => ({
-    counter: state.counter + props.increment
-  }));
-
-  // Correct
-this.setState(function(state, props) {
-    return {
-      counter: state.counter + props.increment
-    };
-  });
 
 ////
 enum Color {
@@ -47,18 +41,6 @@ enum Color {
 //const colorEnum = "Blue" as Color won't error, and you would be left thinking that colorEnum is OK. But if you were to console.log it, you'd see "Blue"
 const color = "Green";
 const colorEnum = color as Color;
-
-////
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-  };
-  
-  const element = (
-    <h1>
-      Hello, {formatName(user)}!
-    </h1>
-  );
 
 <Parent key="value">
               <Children />
